f2d25ab495afacab1c1c85019bb13bcb8b73546b
PietroDanieli <1pie7d@gmail.com>
2022-09-03 13:35:43 +0200

    implement random number

--- js/script.js
+++ js/script.js
@@ -15,17 +15,23 @@ inputHTML.addEventListener("click", function () {
   // rendiamo visibile il tutto
   grid.classList.remove("d-none");
 
+  // creo un array dove inserire i miei numeri fino al 100
+
+  const myNumbersSquare = [];
+  for (let index = 1; index <= 100; index++) {
+    myNumbersSquare.push(index);
+  }
+  const myNumbersSquareSort = myNumbersSquare.sort();
   // creiamo con un ciclo 100 elementi creando una variabile locale
 
-  for (let squareIndex = 1; squareIndex <= 100; squareIndex++) {
+  for (let squareIndex = 0; squareIndex < 100; squareIndex++) {
     // nascondo l'input per evitarne altri
 
     // creo degli elementi div
     const mySquare = document.createElement("div");
 
-    mySquare.append(squareIndex);
     mySquare.classList.add("my-square");
-
+    mySquare.append(myNumbersSquareSort[squareIndex]);
     // /condition for output
 
     mySquare.addEventListener("click", function () {
@@ -42,6 +48,6 @@ inputHTML.addEventListener("click", function () {
 
     grid.append(mySquare);
   }
-
+  console.log(myNumbersSquare);
   // End code
 });
